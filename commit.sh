d2a9f67e947dda81f7181b7efb480169144ab7c2
Donkey Mad <BigMadDonkey@outlook.com>
2024-06-07 16:21:03 +0800

    fix auto commit sh

--- commit.sh
+++ commit.sh
@@ -1,2 +1,2 @@
 echo ready to commit with message: $1
-git -c user.name='Donkey Mad' -c user.email='BigMadDonkey@outlook.com' commit -m $1
+git -c user.name='Donkey Mad' -c user.email='BigMadDonkey@outlook.com' commit -m "$1"
